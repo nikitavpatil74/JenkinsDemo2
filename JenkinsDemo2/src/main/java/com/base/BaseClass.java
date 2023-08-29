7b39186cc55edaa7319b22f2046345cbb1e1fd9d
nikitavpatil74 <135006882+nikitavpatil74@users.noreply.github.com>
2023-08-29 11:51:35 +0000

    Update BaseClass.java

--- JenkinsDemo2/src/main/java/com/base/BaseClass.java
+++ JenkinsDemo2/src/main/java/com/base/BaseClass.java
@@ -1,5 +1,6 @@
 package com.base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
